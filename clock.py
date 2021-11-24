83a1c74e839cfe2ad47075dbbe82d79efeb2aea0
ppppp <57586299+Georgi19@users.noreply.github.com>
2021-11-24 08:12:42 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 from main.jobs import update_movies_db,update_people_db,add_cast,add_details_people,add_genres,delete_new
 import os
 from datetime import datetime
